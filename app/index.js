1a82c93f80066a4da35700705cd263522a1e0bd0
EugeneSokolyansky <evgenys@123software.ru>
2018-01-22 17:33:51 +0200

    first upload

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import React,{Component}  from  'react';
+import ReactDom from 'react-dom';
+import './index.css';
+import App from './app.js';
+
+
+
+
+ReactDom.render(
+    <App />,
+    document.getElementById('app')
+);
